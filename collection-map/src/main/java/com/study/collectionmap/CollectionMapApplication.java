aa068447d0f9971c8e3403d7a4910d145a507774
xiajun <xiajun@dycjr.com>
2019-05-29 17:42:57 +0800

    集合

---
+++ collection-map/src/main/java/com/study/collectionmap/CollectionMapApplication.java
@@ -0,0 +1,13 @@
+package com.study.collectionmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollectionMapApplication.class, args);
+    }
+
+}
