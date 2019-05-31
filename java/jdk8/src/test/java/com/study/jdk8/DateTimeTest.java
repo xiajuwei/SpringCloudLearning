eb5c9f8de7e3a1f2bad98552e890ec90c6c83594
xiajun <xiajun@dycjr.com>
2019-05-31 11:13:29 +0800

    删除

--- java/jdk8/src/test/java/com/study/jdk8/DateTimeTest.java
+++ java/jdk8/src/test/java/com/study/jdk8/DateTimeTest.java
@@ -1,4 +1,23 @@
 package com.study.jdk8;
 
+import org.joda.time.DateTime;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
 public class DateTimeTest {
+
+    @Test
+    public void test(){
+        DateTime dateTime = DateTime.now();
+        System.out.println(dateTime);
+
+        System.out.println(dateTime.toString("yyyy-mm-dd HH:MM:SS"));
+
+
+    }
+
 }
