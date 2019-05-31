cb444913bef40c9649bad0d044307a4ca81002aa
xiajun <xiajun@dycjr.com>
2019-05-31 17:51:54 +0800

    配置

--- spring-cloud/eureka-feign/src/main/java/com/study/springcloud/eurekafeign/controller/EurekaFeignController.java
+++ spring-cloud/eureka-feign/src/main/java/com/study/springcloud/eurekafeign/controller/EurekaFeignController.java
@@ -1,14 +1,15 @@
 package com.study.springcloud.eurekafeign.controller;
 
 import com.study.springcloud.eurekafeign.feign.EurekaFeign;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @RestController
 public class EurekaFeignController {
-    @Autowired
+    @Resource
     EurekaFeign eurekaFeign;
 
     @RequestMapping("/hi")
