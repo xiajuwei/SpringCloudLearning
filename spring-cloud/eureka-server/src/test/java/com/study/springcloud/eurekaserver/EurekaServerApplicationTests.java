8bd8d88dd1200054f39a2d582d841925608ccb27
xiajun <xiajun@dycjr.com>
2019-06-01 21:34:13 +0800

    还原

--- spring-cloud/eureka-server/src/test/java/com/study/springcloud/eurekaserver/EurekaServerApplicationTests.java
+++ spring-cloud/eureka-server/src/test/java/com/study/springcloud/eurekaserver/EurekaServerApplicationTests.java
@@ -1,6 +1,5 @@
 package com.study.springcloud.eurekaserver;
 
-import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,7 +7,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@Slf4j
 public class EurekaServerApplicationTests {
 
     @Test
