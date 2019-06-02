e9727af7bc580341e987561ec0daf8e63ad961d7
xiajun <xiajun@dycjr.com>
2019-06-02 20:48:50 +0800

    消息刷新'

--- spring-cloud/config-client/src/main/java/com/study/configclient/controller/ConfigClientController.java
+++ spring-cloud/config-client/src/main/java/com/study/configclient/controller/ConfigClientController.java
@@ -1,18 +1,24 @@
 package com.study.configclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ConfigClientController {
 
     @Value("${hello}")
     String hello;
+    @Value("${test}")
+    String test;
 
     @RequestMapping(value = "/hi")
     public String hi() {
-        return hello;
+        return hello + test;
     }
 
 
