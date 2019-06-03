3a8d95ce515a6807711512ef81c174ad906c5952
xiajun <xiajun@dycjr.com>
2019-06-03 22:51:10 +0800

    rabbiqmq

---
+++ spring/spring-rabbitmq/src/test/java/com/study/springrabbitmq/SpringRabbitmqApplicationTests.java
@@ -0,0 +1,44 @@
+package com.study.springrabbitmq;
+
+import com.study.springrabbitmq.send.Sender;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringRabbitmqApplicationTests {
+    @Resource
+    private Sender sender;
+
+
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void test1() {
+        sender.send1();
+
+    }
+
+    @Test
+    public void test2() {
+        for (int i = 0; i < 100000; i++) {
+            sender.send2();
+        }
+
+    }
+   @Test
+    public void test3() {
+        for (int i = 0; i < 10000; i++) {
+            sender.send3();
+        }
+
+    }
+
+
+}
