022889a30f10fb3b18e7946ff2dc075c010343df
xiajun <xiajun@dycjr.com>
2019-06-17 17:43:42 +0800

    缓存区

--- java/io-nio/src/test/java/com/study/java/ionio/IOTest.java
+++ java/io-nio/src/test/java/com/study/java/ionio/IOTest.java
@@ -1,4 +1,74 @@
 package com.study.java.ionio;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.*;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
 public class IOTest {
+    @Test
+    public void test1() {
+        InputStream in = null;
+        OutputStream out = null;
+
+        try {
+            in = new FileInputStream("E:/io-nio-test/io.txt");
+            out = new FileOutputStream("E:/io-nio-test/io-write.txt");
+            byte[] bytes = new byte[1024];
+
+            int count = in.read(bytes);
+            while (count != -1) {
+                for (int i = 0; i < count; i++) {
+                    System.out.print((char) bytes[i]);
+                }
+                out.write(bytes);
+                count = in.read(bytes);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                in.close();
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Test
+    public void test2() {
+        Reader reader = null;
+        Writer writer = null;
+        try {
+            reader = new FileReader("E:/io-nio-test/io.txt");
+            writer = new FileWriter("E:/io-nio-test/io-write.txt");
+            char[] chars = new char[1024];
+            int count = reader.read(chars);
+            while (count != -1) {
+                for (int i = 0; i < count; i++) {
+                    System.out.print(chars[i]);
+                }
+                writer.write(chars);
+                count = reader.read(chars);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
 }
--- java/io-nio/src/test/java/com/study/java/ionio/NIOTest.java
+++ java/io-nio/src/test/java/com/study/java/ionio/NIOTest.java
@@ -7,6 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.*;
 import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 
 @SpringBootTest
@@ -23,12 +24,13 @@ public class NIOTest {
             ByteBuffer buffer = ByteBuffer.allocate(1024);
             int count = inChannel.read(buffer);
             while (count != -1) {
+                while (buffer.hasRemaining()) {
+                    System.out.print((char) buffer.get());
+                }
+
                 buffer.flip();
                 FileChannel outChannel = out.getChannel();
                 outChannel.write(buffer);
-                while (buffer.hasRemaining()) {
-                    System.out.println((char) buffer.get());
-                }
                 buffer.compact();
                 count = inChannel.read(buffer);
             }
@@ -46,16 +48,149 @@ public class NIOTest {
 
     @Test
     public void test2() {
-        Reader reader = null;
-        Writer writer = null;
+        RandomAccessFile readerFile = null;
+        RandomAccessFile writerFile = null;
 
         try {
-            reader = new FileReader("E:/io-nio-test/nio.txt");
-            writer = new FileWriter("E:/io-nio-test/nio-write.txt");
+            readerFile = new RandomAccessFile("F:/迅雷下载/deepin-15.10.1-amd64.iso", "rw");
+            writerFile = new RandomAccessFile("F:/迅雷下载/deepin.iso", "rw");
+            //通道
+            FileChannel readerChannel = readerFile.getChannel();
+            FileChannel writerChannel = writerFile.getChannel();
+
+            long start = System.currentTimeMillis();
+            //分配堆内存缓存区空间
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+            int count = readerChannel.read(buffer);
+            while (count != -1) {
+                buffer.flip();
+                while (buffer.hasRemaining()) {
+                    writerChannel.write(buffer);
+                }
+                buffer.compact();
+                count = readerChannel.read(buffer);
+            }
+            long end = System.currentTimeMillis();
+            System.out.println("运行时间：" + (end - start) + "ms");
+
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                readerFile.close();
+                writerFile.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 处理大文件
+     */
+
+    @Test
+    public void test3() {
+        RandomAccessFile readerFile = null;
+        RandomAccessFile writerFile = null;
+
+        try {
+            readerFile = new RandomAccessFile("F:/迅雷下载/deepin-15.10.1-amd64.iso", "rw");
+            writerFile = new RandomAccessFile("F:/迅雷下载/deepin.iso", "rw");
+            long start = System.currentTimeMillis();
+            //通道
+            FileChannel readerChannel = readerFile.getChannel();
+            FileChannel writerChannel = writerFile.getChannel();
+            //将文件直接映射到内存（这里的内存指的是虚拟内存，并不是物理内存）
+            ByteBuffer buffer = readerChannel.map(FileChannel.MapMode.READ_WRITE, 0, 1024);
+            int count = readerChannel.read(buffer);
+            while (count != -1) {
+                //position 设回0，limit 设成position 的值
+                buffer.flip();
+                writerChannel.write(buffer);
+                buffer.compact();
+                count = readerChannel.read(buffer);
+            }
+            long end = System.currentTimeMillis();
+            System.out.println("运行时间：" + (end - start) + "ms");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                readerFile.close();
+                writerFile.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    /**
+     * transferFrom
+     */
+    @Test
+    public void test4() {
+        RandomAccessFile readerFile = null;
+        RandomAccessFile writerFile = null;
+
+        try {
+            readerFile = new RandomAccessFile("F:/迅雷下载/deepin-15.10.1-amd64.iso", "rw");
+            writerFile = new RandomAccessFile("F:/迅雷下载/deepin.iso", "rw");
+            long start = System.currentTimeMillis();
+            //通道
+            FileChannel readerChannel = readerFile.getChannel();
+            FileChannel writerChannel = writerFile.getChannel();
+            writerChannel.transferFrom(readerChannel, 0, readerFile.length());
+            long end = System.currentTimeMillis();
+            System.out.println("运行时间：" + (end - start) + "ms");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                readerFile.close();
+                writerFile.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
 
+
     }
+
+    /**
+     * transferTo
+     */
+    @Test
+    public void test5() {
+        RandomAccessFile readerFile = null;
+        RandomAccessFile writerFile = null;
+
+        try {
+            readerFile = new RandomAccessFile("F:/迅雷下载/deepin-15.10.1-amd64.iso", "rw");
+            writerFile = new RandomAccessFile("F:/迅雷下载/deepin.iso", "rw");
+            long start = System.currentTimeMillis();
+            //通道
+            FileChannel readerChannel = readerFile.getChannel();
+            FileChannel writerChannel = writerFile.getChannel();
+            readerChannel.transferTo(0, readerFile.length(), writerChannel);
+            long end = System.currentTimeMillis();
+            System.out.println("运行时间：" + (end - start) + "ms");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                readerFile.close();
+                writerFile.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+
+    }
+
 
 }
